19be5823a3f0f6c0b742675b15104fd2654c7b7a
Mahdi <mahdi.mousavi0011@gmail.com>
2024-10-03 14:55:45 +0200

    lots of change

--- Public/app.js
+++ Public/app.js
@@ -1,5 +1,5 @@
 // state- Skapa array med objekt(monster)
-state = {
+const state = {
   select:[
    {type: "Maritime Monster", name: "Kraken", color: "blue", size: 50, eyeNum: 256, viciousness: 100, headNum: 4},
    {type: "Terrestrial Beast", name: "Mudfang",color: "Red", size: 80, eyeNum: 2,viciousness: 10, headNum: 700},
@@ -13,5 +13,24 @@ state = {
 }
 
 //render()
+const render=() => {
+const card = document.querySelector(".cards");
+
+
+
+for (const m of state.select) {
+    const monster = document.createElement("div");
+
+    monster.innerHTML = `<h2> ${m.name} </h2>.<p> Type: ${m.type}. <p> Color: ${m.color}. <p> Size: ${m.size}. <p> Eye Amount: ${m.eyeNum}. <p> Viciousness: ${m.viciousness}  <p>  Head Amount: ${m.headNum}   `
+
+    card.appendChild(monster);
+}
+
+
+
+
+}
+render();
 
 //app
+
